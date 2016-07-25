257125bf24ba2ae7c760a6ca6299043dbd18066a
Pedro Vicente Gomez <pedro@gokarumi.com>
2016-07-25 16:21:15 +0200

    Remove example tests and start working on our main feature, the MorseTranslator

---
+++ app/dotto/MorseTranslator.scala
@@ -0,0 +1,13 @@
+package dotto
+
+/**
+  * Translates a String passed as parameter into the morse representation. The rules followed to implement this
+  * translator can be found here: https://en.wikipedia.org/wiki/Morse_code
+  */
+object MorseTranslator {
+
+  def toMorse(text: String): String = {
+    ""
+  }
+
+}
---
+++ test/MorseTranslatorSpec.scala
@@ -0,0 +1,9 @@
+import org.scalatest.{Matchers, FlatSpec}
+
+class MorseTranslatorSpec extends FlatSpec with Matchers {
+
+  "toMorse" should "" in {
+
+  }
+
+}
