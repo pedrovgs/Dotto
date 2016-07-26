95b4165b21fe9bc62a0cd8ab717210593eea1b2d
pedrovgs <pedrovicente.gomez@gmail.com>
2016-07-26 22:00:49 +0200

    Create the first view and configure the DottoController to be able to create a navigation flow posting messages in a form

--- app/controllers/DottoController.scala
+++ app/controllers/DottoController.scala
@@ -4,6 +4,7 @@ import javax.inject._
 
 import play.api.data.Forms._
 import play.api.data._
+import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc._
 
 /**
@@ -11,7 +12,9 @@ import play.api.mvc._
   * message into morse.
   */
 @Singleton
-class DottoController @Inject() extends Controller {
+class DottoController @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
+
+  private val messageForm = Form("message" -> nonEmptyText)
 
   /**
     * Main application endpoint. All the requests to the root domain path will arrive here.
@@ -19,7 +22,7 @@ class DottoController @Inject() extends Controller {
     * @return the main page ready to start sending messages from a text box.
     */
   def index = Action {
-    Ok(views.html.index("", ""))
+    Ok(views.html.index(messageForm))
   }
 
   /**
@@ -28,12 +31,12 @@ class DottoController @Inject() extends Controller {
     * @return 201 if the message has been enqueued.
     */
   def toMorse = Action { implicit request =>
-    Form("message" -> nonEmptyText).bindFromRequest.fold(
+    messageForm.bindFromRequest.fold(
       formWithErrors => {
-        BadRequest(views.html.index(errorMessage = "The request sent hasn't the correct structure."))
+        BadRequest(views.html.index(formWithErrors))
       },
-      message => {
-        Created(views.html.index(message = message))
+      messagePosted => {
+        Created(views.html.index(messageForm, messagePosted))
       }
     )
   }
