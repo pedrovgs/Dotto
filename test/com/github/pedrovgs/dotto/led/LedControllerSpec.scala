31d93b925a890ecaf52f65951c6794e07b116292
pedrovgs <pedrovicente.gomez@gmail.com>
2016-08-07 16:05:51 +0200

    Fix broken tests due to the api change

--- test/com/github/pedrovgs/dotto/led/LedControllerSpec.scala
+++ test/com/github/pedrovgs/dotto/led/LedControllerSpec.scala
@@ -3,13 +3,15 @@ package com.github.pedrovgs.dotto.led
 import com.github.pedrovgs.dotto.morse.MorseAlphabet.{Dash, Dot, MorseSymbol, Space}
 import org.scalatest.FlatSpec
 
+import scala.concurrent.duration._
+
 class LedControllerSpec extends FlatSpec {
 
   private val AnyLed = Led17
-  private val AnyMorseSymbolSeq = Seq(Dot, Dot, Dash, Dash, Space, Dot, Dash, Dot)
+  private val AnyMorseSymbolSeq = Seq(Seq(Dot, Dot, Dash), Seq(Dash, Space, Dot, Dash, Dot))
 
   it should "return an empty sequence if the input is an empty sequence" in {
-    val emptySentence = Seq[MorseSymbol]()
+    val emptySentence = Seq[Seq[MorseSymbol]]()
 
     val result = LedController.toLedInteractions(emptySentence, AnyLed)
 
@@ -20,20 +22,20 @@ class LedControllerSpec extends FlatSpec {
     val result = LedController.toLedInteractions(AnyMorseSymbolSeq, AnyLed)
     val oddLedInteractions = Iterator.from(1, 2).takeWhile(_ < result.size).map(result(_))
 
-    assert(result.size == AnyMorseSymbolSeq.size * 2)
+    assert(result.size == AnyMorseSymbolSeq.flatten.size * 2)
     assert(oddLedInteractions.forall {
-      case LedInteraction(AnyLed, Low, 0.5) => true
+      case LedInteraction(AnyLed, Low, _) => true
       case _ => false
     })
   }
 
   it should "return transform even symbols into led interactions" in {
-    val symbols = Seq(Dot, Dash, Space)
+    val symbols = Seq(Seq(Dot, Dash, Space))
     val result = LedController.toLedInteractions(symbols, AnyLed)
 
-    assert(result(0) == LedInteraction(AnyLed, High, 0.5))
-    assert(result(2) == LedInteraction(AnyLed, High, 1.5))
-    assert(result(4) == LedInteraction(AnyLed, High, 4.5))
+    assert(result(0) == LedInteraction(AnyLed, High, 0.1 seconds))
+    assert(result(2) == LedInteraction(AnyLed, High, 0.3 seconds))
+    assert(result(4) == LedInteraction(AnyLed, High, 0.9 seconds))
   }
 
 }
