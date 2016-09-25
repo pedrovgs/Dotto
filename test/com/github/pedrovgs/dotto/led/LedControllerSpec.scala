31cebb775b143752ebdacead8ceadaf556054d46
Pedro Vicente Gomez <pedro@gokarumi.com>
2016-09-25 17:21:20 +0200

    Improve ledController usage adding the _ import

--- test/com/github/pedrovgs/dotto/led/LedControllerSpec.scala
+++ test/com/github/pedrovgs/dotto/led/LedControllerSpec.scala
@@ -1,7 +1,7 @@
 package com.github.pedrovgs.dotto.led
 
 import com.github.pedrovgs.dotto.core.types._
-import com.github.pedrovgs.dotto.interpreter.led.ledController
+import com.github.pedrovgs.dotto.interpreter.led.ledController._
 import org.scalatest.FlatSpec
 
 import scala.concurrent.duration._
@@ -13,13 +13,13 @@ class LedControllerSpec extends FlatSpec {
   it should "return an empty sequence if the input is an empty sequence" in {
     val emptySentence = Seq[Seq[MorseSymbol]]()
 
-    val result = ledController.toLedInteractions(emptySentence)
+    val result = toLedInteractions(emptySentence)
 
     assert(result.isEmpty)
   }
 
   it should "return all the morse symbols separated separated by low interactions" in {
-    val result = ledController.toLedInteractions(AnyMorseSymbolSeq)
+    val result = toLedInteractions(AnyMorseSymbolSeq)
     val oddLedInteractions = Iterator.from(1, 2).takeWhile(_ < result.size).map(result(_))
 
     assert(result.size == AnyMorseSymbolSeq.flatten.size * 2)
@@ -31,7 +31,7 @@ class LedControllerSpec extends FlatSpec {
 
   it should "return transform even symbols into led interactions" in {
     val symbols = Seq(Seq(Dot, Dash, Space))
-    val result = ledController.toLedInteractions(symbols)
+    val result = toLedInteractions(symbols)
 
     assert(result(0) == LedInteraction(High, 0.1 seconds))
     assert(result(2) == LedInteraction(High, 0.3 seconds))
